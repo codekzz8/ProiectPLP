af49e33571a44a3e6b3b9af43a37a9650dbced90
Chitic Ștefan <58853992+codekzz8@users.noreply.github.com>
2020-12-12 17:30:19 +0200

    v3

--- proiect.v
+++ proiect.v
@@ -11,6 +11,8 @@ Local Open Scope list_scope.
 Scheme Equality for list.
 Import ListNotations.
 
+Compute nth 0 [1; 2].
+
 Require Export BinNums.
 Require Import BinPos BinNat.
 Local Open Scope Z_scope.
@@ -38,6 +40,7 @@ Scheme Equality for Results.
 Inductive AExp :=
 | aint : Z -> AExp
 | avar : string -> AExp
+| arrayVal : string -> nat -> AExp
 | aplus : AExp -> AExp -> AExp
 | aminus : AExp -> AExp -> AExp
 | amul : AExp -> AExp -> AExp
@@ -47,9 +50,14 @@ Inductive AExp :=
 | amin : AExp -> AExp -> AExp
 | apow : AExp -> AExp -> AExp.
 
+Coercion aint : Z >-> AExp.
+Coercion avar : string >-> AExp.
+
 Inductive BExp :=
+| berr
 | btrue : BExp
 | bfalse : BExp
+| bvar : string -> BExp
 | beq : AExp -> AExp -> BExp
 | blt : AExp -> AExp -> BExp
 | ble : AExp -> AExp -> BExp
@@ -59,11 +67,14 @@ Inductive BExp :=
 | band : BExp -> BExp -> BExp
 | bor : BExp -> BExp -> BExp.
 
+Coercion bvar : string >-> BExp.
+
 Inductive Stmt :=
 | assignment_int : string -> AExp -> Stmt
+| assignment_array : string -> nat -> AExp -> Stmt
 | assignment_bool : string -> BExp -> Stmt
 | adecboolnull : string -> Stmt
-| adecbool : string -> bool -> Stmt
+| adecbool : string -> BExp -> Stmt
 | adecintnull : string -> Stmt
 | adecint : string -> AExp -> Stmt
 | adecarray : string -> AExp -> Stmt
@@ -80,9 +91,6 @@ Inductive Stmt :=
 | switch : AExp -> Stmt -> Stmt (* AExp -> case_list -> Stmt *)
 | label : string -> Stmt -> Stmt
 | goto : string -> Stmt.
-
-Coercion aint : Z >-> AExp.
-Coercion avar : string >-> AExp.
 
 Notation "A +' B" := (aplus A B) (at level 50).
 Notation "A -' B" := (aminus A B) (at level 50).
@@ -123,15 +131,13 @@ Notation "A '||'' B" := (bor A B) (at level 55).
 Check btrue &&' bfalse.
 Check btrue ||' bfalse.
 
-Notation "'boolean*' A" := (adecboolnull A) (at level 56).
-Notation "'boolean' A =' B" := (adecbool A B) (at level 56).
-
-Check boolean* "ok".
-Check boolean "ok" =' false.
 
 Notation "'int*' A" := (adecintnull A) (at level 50).
 Notation "'int' A =' B" := (adecint A B) (at level 50).
+Notation "'boolean*' A" := (adecboolnull A) (at level 56).
+Notation "'boolean' A =' B" := (adecbool A B) (at level 56).
 Notation "'array' A '['' B '']'" := (adecarray A B) (at level 50).
+Notation "X '#' i ::=a A" := (assignment_array X i A) (at level 50).
 Notation "X ::=n A" := (assignment_int X A) (at level 50).
 Notation "X ::=b A" := (assignment_bool X A) (at level 50).
 Notation "S1 ;; S2" := (sequence S1 S2) (at level 90).
@@ -141,9 +147,14 @@ Notation "'While' '(' A ')' '(' B ')'" := (whilethisdothat A B) (at level 90).
 Notation "'For' '(' A ; B ; C ')' '(' D ')'" := (forthisdothat A B C D) (at level 90).
 Notation "A :' B" := (label A B) (at level 90).
 
+Check boolean* "ok".
+Check boolean "ok" =' bfalse.
+
 Check int* "sum".
 Check int "sum" =' 0.
-Check array "a" ['4'].
+Check array "a"['"MAX"'].
+Check array "b"['10'].
+Check "a"#0 ::=a 3.
 Check define_int "MAX" 1000.
 Check define_bool "OK" btrue.
 Check "sum" ::=n 51.
@@ -262,10 +273,207 @@ Definition mod_err (n1 n2 : ErrorInt) : ErrorInt :=
 Compute mod_err 4 0.
 Compute mod_err 35 2.
 
+Definition eq_err (n1 n2 : ErrorInt) : ErrorBool :=
+match n1, n2 with
+| err_int, _ => err_bool
+| _, err_int => err_bool
+| num v1, num v2 => bbool (Z.eqb v1 v2)
+end.
+Check eq_err 2 2.
 
+Definition lt_err (n1 n2 : ErrorInt) : ErrorBool :=
+match n1, n2 with
+| err_int, _ => err_bool
+| _, err_int => err_bool
+| num v1, num v2 => bbool (Z.ltb v1 v2)
+end.
+Check lt_err 5 2.
 
+Definition le_err (n1 n2 : ErrorInt) : ErrorBool :=
+match n1, n2 with
+| err_int, _ => err_bool
+| _, err_int => err_bool
+| num v1, num v2 => bbool (Z.leb v1 v2)
+end.
+Check le_err 1 1.
 
+Definition gt_err (n1 n2 : ErrorInt) : ErrorBool :=
+match n1, n2 with
+| err_int, _ => err_bool
+| _, err_int => err_bool
+| num v1, num v2 => bbool (Z.gtb v1 v2)
+end.
+Check gt_err 5 2.
 
+Definition ge_err (n1 n2 : ErrorInt) : ErrorBool :=
+match n1, n2 with
+| err_int, _ => err_bool
+| _, err_int => err_bool
+| num v1, num v2 => bbool (Z.geb v1 v2)
+end.
+Check gt_err 6 6.
+
+Definition not_err (n : ErrorBool) : ErrorBool :=
+match n with
+| err_bool => err_bool
+| bbool v => bbool (negb v)
+end.
+Check not_err true.
+
+Definition and_err (n1 n2 : ErrorBool) : ErrorBool :=
+match n1, n2 with
+| err_bool, _ => err_bool
+| _, err_bool => err_bool
+| bbool v1, bbool v2 => bbool (andb v1 v2)
+end.
+Check and_err true false.
+
+Definition or_err (n1 n2 : ErrorBool) : ErrorBool :=
+match n1, n2 with
+| err_bool, _ => err_bool
+| _, err_bool => err_bool
+| bbool v1, bbool v2 => bbool (orb v1 v2)
+end.
+Check or_err true false.
+
+Reserved Notation "A =[ S ]=> N" (at level 65).
+Inductive aeval : AExp -> Env -> ErrorInt -> Prop :=
+| const : forall n sigma, aint n =[ sigma ]=> n
+| var : forall v sigma, avar v =[ sigma ]=> match (sigma v) with
+                                            | rint x => x
+                                            | _ => err_int
+                                            end
+| add' : forall a1 a2 i1 i2 sigma n,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    n = plus_err i1 i2 ->
+    a1 +' a2 =[ sigma ]=> n
+| sub' : forall a1 a2 i1 i2 sigma n,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    n = minus_err i1 i2 ->
+    a1 -' a2 =[ sigma ]=> n
+| mul' : forall a1 a2 i1 i2 sigma n,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    n = mul_err i1 i2 ->
+    a1 *' a2 =[sigma]=> n
+| div' : forall a1 a2 i1 i2 sigma n,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    n = div_err i1 i2 ->
+    a1 /' a2 =[ sigma ]=> n
+| mod' : forall a1 a2 i1 i2 sigma n,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    n = mod_err i1 i2 ->
+    a1 %' a2 =[ sigma ]=> n
+where "a =[ sigma ]=> n" := (aeval a sigma n).
+
+Reserved Notation "B ={ S }=> B'" (at level 70).
+Inductive beval : BExp -> Env -> ErrorBool -> Prop :=
+| e_err : forall sigma, berr ={ sigma }=> err_bool
+| e_true : forall sigma, btrue ={ sigma }=> true
+| e_false : forall sigma, bfalse ={ sigma }=> false
+| e_equal : forall a1 a2 i1 i2 sigma b,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    b = eq_err i1 i2 ->
+    a1 ==' a2 ={ sigma }=> b
+| e_lessthan : forall a1 a2 i1 i2 sigma b,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    b = lt_err i1 i2 ->
+    a1 <' a2 ={ sigma }=> b
+| e_lessequalthan : forall a1 a2 i1 i2 sigma b,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    b = le_err i1 i2 ->
+    a1 <=' a2 ={ sigma }=> b
+| e_greaterthan : forall a1 a2 i1 i2 sigma b,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    b = gt_err i1 i2 ->
+    a1 >' a2 ={ sigma }=> b
+| e_greaterequalthan : forall a1 a2 i1 i2 sigma b,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    b = ge_err i1 i2 ->
+    a1 >=' a2 ={ sigma }=> b
+| e_not : forall a1 i1 sigma b,
+    a1 ={ sigma }=> i1 ->
+    b = (not_err i1) ->
+    ! a1 ={ sigma }=> b
+| e_and : forall a1 a2 i1 i2 sigma b,
+    a1 ={ sigma }=> i1 ->
+    a2 ={ sigma }=> i2 ->
+    b = (and_err i1 i2) ->
+    (a1 &&' a2) ={ sigma }=> b
+| e_or : forall a1 a2 i1 i2 sigma b,
+    a1 ={ sigma }=> i1 ->
+    a2 ={ sigma }=> i2 ->
+    b = (or_err i1 i2) ->
+    (a1 ||' a2) ={ sigma }=> b
+where "B ={ S }=> B'" := (beval B S B').
+
+Reserved Notation "S -{ sigma }-> Sigma'" (at level 60).
+Inductive eval : Stmt -> Env -> Env -> Prop :=
+| e_int_decl_null : forall x sigma sigma',
+  sigma' = (update sigma x (rint 0)) ->
+  (int* x) -{ sigma }-> sigma'
+| e_int_decl : forall a i x sigma sigma',
+  a =[ sigma ]=> i ->
+  sigma' = (update sigma x (rint i)) ->
+  (int x =' a) -{ sigma }-> sigma'
+| e_int_assign : forall a i x sigma sigma',
+  a =[ sigma ]=> i ->
+  sigma' = (update sigma x (rint i)) ->
+  (x ::=n a) -{ sigma }-> sigma'
+| e_bool_decl_null : forall a x sigma sigma',
+  sigma' = (update sigma x (rbool false)) ->
+  (boolean* a) -{ sigma }-> sigma'
+| e_bool_decl : forall a i x sigma sigma',
+  a ={ sigma }=> i ->
+  sigma' = (update sigma x (rbool i)) ->
+  (boolean x =' a) -{ sigma }-> sigma'
+| e_bool_assign : forall a i x sigma sigma',
+  a ={ sigma }=> i ->
+  sigma' = (update sigma x (rbool i)) ->
+  (x ::=b a) -{ sigma }-> sigma'
+| e_seq : forall s1 s2 sigma sigma1 sigma2,
+  s1 -{ sigma }-> sigma1 ->
+  s2 -{ sigma }-> sigma2 ->
+  (s1 ;; s2) -{ sigma }-> sigma2
+| e_ifthen_false : forall b s1 sigma,
+    b ={ sigma }=> false ->
+    (Iff b Then s1) -{ sigma }-> sigma
+| e_ifthen_true : forall b s1 sigma sigma1,
+    b ={ sigma }=> true ->
+    s1 -{ sigma }-> sigma1 ->
+    (Iff b Then s1) -{ sigma }-> sigma1
+| e_ifthenelse_false : forall b s1 s2 sigma sigma2,
+    b ={ sigma }=> false ->
+    s2 -{ sigma }-> sigma2 ->
+    (If b Then s1 Else s2) -{ sigma }-> sigma2
+| e_ifthenelse_true : forall b s1 s2 sigma sigma1,
+    b ={ sigma }=> true ->
+    s1 -{ sigma }-> sigma1 ->
+    (If b Then s1 Else s2) -{ sigma }-> sigma1
+| e_whilefalse : forall b s sigma,
+    b ={ sigma }=> false ->
+    whilethisdothat b s -{ sigma }-> sigma
+| e_whiletrue : forall b s sigma sigma',
+    b ={ sigma }=> true ->
+    (s ;; whilethisdothat b s) -{ sigma }-> sigma' ->
+    whilethisdothat b s -{ sigma }-> sigma'
+| e_forfalse : forall e1 e2 e3 s sigma,
+    e2 ={ sigma }=> false ->
+    forthisdothat e1 e2 e3 s -{ sigma }-> sigma
+| e_fortrue : forall e1 e2 e3 s sigma sigma',
+    e2 ={ sigma }=> true ->
+    (e1 ;; whilethisdothat e2 (s ;; e3)) -{ sigma }-> sigma' ->
+    forthisdothat e1 e2 e3 s -{ sigma }-> sigma'
+where "S -{ sigma }-> Sigma'" := (eval S sigma Sigma').
 
 
 
